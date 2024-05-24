3f62ecb6775f846d07d2ddd3a029c0b6f4dac419
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-24 18:51:34 -0300

    changed package json script

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "world",
+    };
+  }
+
   render() {
     return (
       <>
-        <p>hello world</p>
+        <p>hello {this.state.name}</p>
+        <button onClick={() => this.setState({ name: "mom" })}>
+          Press to say hi mom
+        </button>
       </>
     );
   }
