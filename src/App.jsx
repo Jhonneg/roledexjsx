d2da327c9e2e77ab874cec941bf1864368d223c6
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-25 17:56:49 -0300

    added fetch inside component did mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
 import { Component } from "react";
 import "./App.css";
 
-class App extends Component {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
     };
   }
-  componentDidMount() {
-    
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const users = await response.json();
+      this.setState(
+        () => {
+          return { monsters: users };
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
   }
 
   render() {
     return (
-      <>
-        <p>hello {this.state.name}</p>
-        <button onClick={() => this.setState({ name: "mom" })}>
-          Press to say hi mom
-        </button>
-      </>
+      <div>
+        <h1>Monsters go here</h1>
+        <ul>
+          {this.state.monsters.map((monster) => {
+            return <li key={monster.id}>{monster.name}</li>;
+          })}
+        </ul>
+      </div>
     );
   }
 }
-
-export default App;
