1f90510efdb26dbacea900ece7e48eda368ce6bb
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-26 01:53:26 -0300

    app function converted to fc

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,22 @@ import "./App.css";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
 
-function App() {
+export default function App() {
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
+    (async () => {
       try {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users"
         );
-        if (!response.ok) {
-          throw new Error("Failed to fetch");
-        }
         const users = await response.json();
         setMonsters(users);
       } catch (error) {
         console.error("Fetch error:", error);
       }
-    }
-    fetchData();
+    })();
   }, []);
 
   const onSearchChange = (event) => {
@@ -43,46 +39,3 @@ function App() {
     </div>
   );
 }
-//  class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-//   async componentDidMount() {
-//     try {
-//       const response = await fetch(
-//         "https://jsonplaceholder.typicode.com/users"
-//       );
-//       const users = await response.json();
-//       this.setState(() => {
-//         return { monsters: users };
-//       });
-//     } catch (error) {
-//       console.error("Error fetching users:", error);
-//     }
-//   }
-
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter((monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-
-//     return (
-//       <div>
-//         <h1>Search Monsters</h1>
-//         <SearchBox
-//           placeholder="Search monsters"
-//           onSearchChange={onSearchChange}
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
-export default App;
